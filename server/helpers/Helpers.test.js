935f22d649a4f3a2ebe0dfdbcabf10c4197e7907
Kevin Amiranoff <kamiranoff@gmail.com>
2017-07-01 22:15:13 +0200

    update test scripts and add one more test

---
+++ server/helpers/Helpers.test.js
@@ -0,0 +1,12 @@
+import Helpers from './Helpers';
+
+describe('Helpers', () => {
+  const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  describe('shuffle', () => {
+    test('should shuffle the array', () => {
+      const array2 = Helpers.shuffle(array1);
+      expect(array2).toBeInstanceOf(Array);
+      expect(array2).toHaveLength(10);
+    });
+  });
+});
